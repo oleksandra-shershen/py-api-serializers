420b903237004e4747d86b1802e68c8a7098c28b
Shershen Alexandra <hornet240204@gmail.com>
2024-05-26 18:43:14 +0300

    Add serializer, view, endpoint for CinemaHall

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,11 @@
-# write urls here
+from django.urls import path, include
+from cinema.views import CinemaHallViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("cinema_halls", CinemaHallViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
+app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,8 @@
-# write views here
+from rest_framework import viewsets
+from cinema.models import CinemaHall, Genre, Actor, Movie, MovieSession
+from cinema.serializers import CinemaHallSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
